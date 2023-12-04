f140fd2825c8e92dabb6b5a5ee95e275f54919de
Gitano8600 <manuel.nellen@glattnet.ch>
2023-12-04 18:40:33 +0100

    day 4

--- 2023/04/20231204_part1.py
+++ 2023/04/20231204_part1.py
@@ -1,4 +1,17 @@
-
 file = open("input.txt", "r")
 values = file.read().splitlines()
-final_value = 0
+final_sum = 0
+
+values_without_card = [i.split(":")[1] for i in values]
+
+for i in values_without_card:
+    count = 0
+    winning_numbers = (i.split("|")[0].strip()).split()
+    card_numbers = (i.split("|")[1].strip()).split()
+
+    for nbr in winning_numbers:
+        if nbr in card_numbers:
+            count = count * 2 if count != 0 else 1
+    final_sum += count
+
+print(final_sum)
--- 2023/04/20231204_part2.py
+++ 2023/04/20231204_part2.py
@@ -1,4 +1,30 @@
-
 file = open("input.txt", "r")
 values = file.read().splitlines()
-final_value = 0
+final_sum = 0
+dict_of_cards = dict()
+
+for i in values:
+    count = 0
+    card_id = int(i.split(":")[0].lstrip("Card "))
+    winning_numbers = i.split(":")[1].split("|")[0].strip().split()
+    card_numbers = i.split(":")[1].split("|")[1].strip().split()
+
+    for nbr in winning_numbers:
+        if nbr in card_numbers:
+            count += 1
+
+    dict_of_cards[card_id] = {"wins": count,
+                              "instances": 1}
+
+for key, value in dict_of_cards.items():
+    if value['wins'] != 0:
+        for occ in range(value['instances']):
+            counter = key
+            for win in range(value['wins']):
+                counter += 1
+                dict_of_cards[counter]['instances'] += 1
+
+for value in dict_of_cards.values():
+    final_sum += value['instances']
+
+print(final_sum)
