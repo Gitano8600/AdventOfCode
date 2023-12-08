d310757882add3e475cedc319129149661e7f8c1
Gitano8600 <manuel.nellen@glattnet.ch>
2023-12-08 23:29:45 +0100

    day 8

--- 2023/08/20231208_part1.py
+++ 2023/08/20231208_part1.py
@@ -1,4 +1,18 @@
+from collections import OrderedDict
+
 file = open("input.txt", "r")
 values = file.read().splitlines()
-hand_list = []
+node_map = OrderedDict()
 result_value = 0
+
+for i in values[2:]:
+    node_map[i[:3]] = (i[7:10], i[12:15])
+
+curr_waypoint = "AAA"
+
+while curr_waypoint != "ZZZ":
+    for char in values[0]:
+        curr_waypoint = node_map[curr_waypoint][1] if char == "R" else node_map[curr_waypoint][0]
+        result_value += 1
+
+print(result_value)
--- 2023/08/20231208_part2.py
+++ 2023/08/20231208_part2.py
@@ -1,4 +1,25 @@
+from itertools import cycle
+from math import lcm
+
 file = open("input.txt", "r")
 values = file.read().splitlines()
-hand_list = []
-result_value = 0
+
+
+node_map = {i[:3]: (i[7:10], i[12:15]) for i in values[2:]}
+starting_nodes = [i[:3] for i in values[2:] if i[2] == "A"]
+paths = cycle(c=="R" for c in values[0])
+steps = 0
+
+
+def steps_to_goal(node):
+    end_point = lambda waypoint: waypoint[2] == "Z"
+    steps = 0
+    while not end_point(node):
+        node = node_map[node][next(paths)]
+        steps += 1
+    return steps
+
+
+result_value = lcm(*(steps_to_goal(node) for node in starting_nodes))
+
+print(result_value)
