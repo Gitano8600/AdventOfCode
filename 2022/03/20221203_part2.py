499744e199abe3697d2a64fcd369c8a059544f65
Gitano8600 <manu_nellen@hotmail.com>
2024-11-17 20:57:19 +0100

    2022 day 3

--- 2022/03/20221203_part2.py
+++ 2022/03/20221203_part2.py
@@ -4,11 +4,17 @@ file = open("input.txt", "r")
 values = file.read().splitlines()
 final_score = 0
 
+print(values)
+
+def grouper(all_groups, group_size=3):
+    for item in range(0, len(all_groups), group_size):
+        yield all_groups[item:item + group_size]
+
 
 def find_common_char(pattern):
-    pattern1, pattern2 = pattern[:len(pattern)//2], pattern[len(pattern)//2:]
+    pattern1, pattern2, pattern3 = pattern
     regex = re.compile(f"[{re.escape(pattern2)}]")
-    return [char for char in pattern1 if regex.match(char)][0]
+    return [char for char in pattern1 if regex.match(char) and char in pattern3][0]
 
 
 def return_prio(item):
@@ -18,7 +24,8 @@ def return_prio(item):
         return ord(item) - ord('A')+27
 
 
-for value in values:
+for value in grouper(values):
+    print(value)
     final_score += return_prio(find_common_char(value))
 
 print(final_score)
