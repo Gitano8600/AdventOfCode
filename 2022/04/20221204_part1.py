0fdf926a4ef7f6054918315a45efddf09bad688e
Gitano8600 <manu_nellen@hotmail.com>
2024-11-18 18:00:04 +0100

    Refactor 2022 day 4 part 1

--- 2022/04/20221204_part1.py
+++ 2022/04/20221204_part1.py
@@ -1,39 +1,29 @@
+import re
+
 file = open("input.txt", "r")
 values = file.read().splitlines()
 final_value = 0
 
 
-def longer_range(pattern):
-    range1, range2 = pattern.split(',')
-    range1_length = int(range1.split('-')[1]) - int(range1.split('-')[0])
-    range2_length = int(range2.split('-')[1]) - int(range2.split('-')[0])
-    if range1_length > range2_length:
-        return 'first'
-    elif range2_length > range1_length:
-        return 'second'
-    elif (range2_length == range1_length) and (range1 == range2):
-        return 'even'
-    else:
-        return 'other'
-
-
-def is_within(long_pattern, short_pattern):
-    if int(long_pattern.split('-')[1]) >= int(short_pattern.split('-')[1]):
-        if int(long_pattern.split('-')[0]) <= int(short_pattern.split('-')[0]):
-            return True
-    return False
-
-
 for value in values:
-    longer_pattern = longer_range(value)
-    if longer_pattern == 'first':
-        if is_within(value.split(',')[0], value.split(',')[1]):
+    a, b, c, d = re.findall(r"[\w']+", value)
+    print(a, b, c, d)
+    if int(a) < int(c):
+        if int(b) >= int(d):
+            print('hit1')
             final_value += 1
-    elif longer_pattern == 'second':
-        if is_within(value.split(',')[1], value.split(',')[0]):
+    elif int(c) < int(a):
+        if int(d) >= int(b):
+            print('hit2')
             final_value += 1
-    elif longer_pattern == 'even':
-        final_value += 1
+    elif int(a) == int(c):
+        if min(int(b), int(d)) <= max(int(b), int(d)):
+            print('hit3')
+            final_value += 1
+    elif int(a) == int(c):
+        if min(int(b), int(d)) <= max(int(b), int(d)):
+            print('hit4')
+            final_value += 1
 
 
 print(final_value)
